5cb54be45181667bc6fa33ef78656ab5ebafa405
Danattar <danattarxelnaga@gmail.com>
2016-11-29 23:02:32 +0100

    Create Main.h

---
+++ CPP/Main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <string>
+
+using namespace std;
+
+int getInput(string message, int max, int min = 1);
+
+bool inRange(int value, int min, int max);
+
+int main();
+
+#endif //MAIN.H
